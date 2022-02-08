269dca5bfb00c27dd7063b981b2d14dc317326a7
Shimek W <oryginallogin@gmail.com>
2022-02-08 14:44:33 +0100

    add interval activity

---
+++ app/src/main/java/com/example/chudnijdziku/IntervalsActivity.java
@@ -0,0 +1,14 @@
+package com.example.chudnijdziku;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntervalsActivity extends Intervals {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intervals);
+    }
+}
