c99781b673e13e383ae67494c2ea5df39f84ae1d
Shimek W <oryginallogin@gmail.com>
2022-02-17 19:38:09 +0100

    add random infos generator

--- app/src/main/java/com/example/chudnijdziku/MainActivity.java
+++ app/src/main/java/com/example/chudnijdziku/MainActivity.java
@@ -1,17 +1,27 @@
 package com.example.chudnijdziku;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private float bodyweight, growth;
-    private int age, BMO;
+    private float bodyweight = 0, growth = 0;
+    private int age = 0, BMO;
+    private TextView textViewRandomInfos;
+    private static List<Integer> list = new ArrayList<>();
 
     public int getBMO() {
         return BMO;
@@ -53,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN); //enable full screen
         setContentView(R.layout.activity_main);
+        textViewRandomInfos = (TextView) findViewById(R.id.randFactsTextView);
+        showRandomInfo();
+
     }
 
     public void goIntervals(View view) {
@@ -60,9 +73,55 @@ public class MainActivity extends AppCompatActivity {
         startActivity(i);
     }
 
-    public void GoJourney(View view) {
+    public void goJourney(View view) {
         Intent i = new Intent(this, JourneyActivity.class);
         startActivity(i);
+    }
+
+    private String[] randomInfos = new String[] {
+            "Większość diety każdego dzika (około 50%) powinny stanowić zdrowe węglowodany. Oto kilka z nich: " +
+                    "ryż brązowy, kasza gryczana niepalona, kasza jaglana, komosa ryżowa, makarony pełnoziarniste," +
+                    "chleb z pełnej mąki (ciemny), płatki owsiane górskie, soczewica, ziemniaki (gotowane), bataty. " +
+                    "Jest ich znacznie więcej, a wystarczy kierować się główną zasadą: czym mniej przetworzone, tym lepiej. ",
+            "Około 30% żywności w diecie dzika powinny stanowić tłuszcze. Zdrowe tłuszcze to np." +
+                    " awokado, oliwa z oliwek, olej lniany, orzechy i masło orzechowe 100%, nasiona słonecznika, pestki dyni.",
+            "Czy wiesz, ale mały kebab drobiowy w bułce ze wszystkimi dodatkami waży około 700g? To jest prawie 1400 Kcal w jednym posiłku. Mimo to" +
+                    "jest to jeden z najlepszych wyborów jeśli chodzi o fastfoody, gdyż zawiera najlepsze proporcje makroskładników. Jak to się mówi" +
+                    " wszystko jest dla ludzi byle z głową. Na przykład połączenie piwa z kebsem to bardzo zły pomysł. Wątroba musi w pierwszej kolejności wziąć" +
+                    "się za rozłożenie alkoholu, a na kalorie z jedzenia jest już za późno.",
+            "Są trzy makroskładniki: Węglowodany, tłuszcze i białka. Staraj się by w dniu proporcje ich spożycia wynosiły około 5:3:2, to znaczy" +
+                    " na pięć części węglowodanów, trzy części tłuszczy i dwie części białka. Pamiętaj jednak, że to są tylko orientacyjne proporcje." +
+                    " Nic się nie stanie jak jednego dnia zje się więcej jednego czy drugiego. Jedzenie to ma też być przyjemność :) "
+
+    };
+    public int randomInfoNum(){
+        int min = 0;
+        int max = randomInfos.length-1;
+        int result = (int)(Math.random()*(max - min + 1) + min);
+        return result;
+    }
+
+    public void showRandomInfo(){
+        if (list.size() == randomInfos.length - 1){
+            list.clear();
+        }
+        //i will have to send list size to file but i think 'checking' logic is correct
+        int randomIndex =  randomInfoNum();
+        boolean shownIndex = wasItShown(randomIndex, list);
+        if (shownIndex == true){
+           showRandomInfo();
+        }else{
+            list.add(randomIndex);
+            textViewRandomInfos.setText(randomInfos[randomIndex]);
+        }
+    }
+    public boolean wasItShown(int num, List<Integer> list){
+        for(int i : list){
+            if(i == num){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
